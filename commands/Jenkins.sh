7a62f27582cb13ab127acec07e92241a31c37905
Murali <muraliclahan526@gmail.com>
2025-08-07 15:05:08 +0530

    added commands

---
+++ commands/Jenkins.sh
@@ -0,0 +1,23 @@
+# Java JDK 21
+
+sudo apt update
+sudo apt install fontconfig openjdk-21-jre
+
+
+# Jenkins 
+
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins
+
+
+# Start Jenkins
+
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
+
