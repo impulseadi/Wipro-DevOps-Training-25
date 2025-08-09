cc7ed3e665f191a426346abcbac4349a246008de
Msocial123 <Msocial123>
2025-08-09 12:19:48 +0530

    jenkins files updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,7 +18,7 @@ provider "aws" {
 # aws ec2 instance provisoning
 
 resource "aws_instance" "web" {
-  ami           = "ami-0a174b8e659123575"
+  ami           = ""
   instance_type = "t2.small"
 
   tags = {
